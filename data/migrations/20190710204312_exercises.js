417241ac67956fb49c08002897b2e00793ac4416
Mychal <hallmychal@gmail.com>
2019-07-10 21:08:50 -0500

    exercises

--- data/migrations/20190710204312_exercises.js
+++ data/migrations/20190710204312_exercises.js
@@ -1,7 +1,22 @@
 
 exports.up = (knex) =>
-    knex.schema.createTable('exercises')
-
+    knex.schema.createTable('exercises', tbl => {
+        tbl.string('eid')
+        .primary()
+        .unique()
+        .notNullable()
+        tbl.string('uid')
+        .references("uid")
+        .inTable("users")
+        .notNullable()
+        tbl.string('name')
+        .unique()
+        .notNullable()
+        tbl.string('description')
+        .notNullable()
+        tbl.string('icon_src')
+        tbl.timestamps(true, true)
+    })
 exports.down = function(knex) {
-  
+  knex.schema.dropTableIfExists('exercises')
 };
