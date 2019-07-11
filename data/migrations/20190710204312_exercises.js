1a4a7f2b73b8ddcfbb1605bd2a5a78cff942365b
Mychal <hallmychal@gmail.com>
2019-07-10 21:21:25 -0500

    logs

--- data/migrations/20190710204312_exercises.js
+++ data/migrations/20190710204312_exercises.js
@@ -1,22 +1,23 @@
-
-exports.up = (knex) =>
-    knex.schema.createTable('exercises', tbl => {
-        tbl.string('eid')
-        .primary()
-        .unique()
-        .notNullable()
-        tbl.string('uid')
-        .references("uid")
-        .inTable("users")
-        .notNullable()
-        tbl.string('name')
-        .unique()
-        .notNullable()
-        tbl.string('description')
-        .notNullable()
-        tbl.string('icon_src')
-        tbl.timestamps(true, true)
-    })
+exports.up = knex =>
+  knex.schema.createTable("exercises", tbl => {
+    tbl
+      .string("eid")
+      .primary()
+      .unique()
+      .notNullable();
+    tbl
+      .string("uid")
+      .references("uid")
+      .inTable("users")
+      .notNullable();
+    tbl
+      .string("name")
+      .unique()
+      .notNullable();
+    tbl.string("description").notNullable();
+    tbl.string("icon_src");
+    tbl.timestamps(true, true);
+  });
 exports.down = function(knex) {
-  knex.schema.dropTableIfExists('exercises')
+  knex.schema.dropTableIfExists("exercises");
 };
