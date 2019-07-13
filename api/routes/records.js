ec4e3e906984f84efe99020def0954b0ce236428
Mychal <hallmychal@gmail.com>
2019-07-13 12:31:50 -0500

    models

---
+++ api/routes/records.js
@@ -0,0 +1,15 @@
+// records look like this
+
+//  rid pun
+//  uid fn
+//  eid fn
+//  duration
+//  repetitions
+//  distance
+//  heart_rate
+//  weight
+//  calories
+//  notes
+//  datetime
+//  timestamps
+
--- api/server.js
+++ api/server.js
@@ -1,27 +1,29 @@
 //IMPORTS
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 //routes
-const routesUsers = require('./routes/users')
-const routesExercises = require('./routes/exercises')
+const routesUsers = require("./routes/users");
+const routesExercises = require("./routes/exercises");
+const routesRecords = require("./routes/records");
 
 //SETUP
-const server = express()
+const server = express();
 
 //MIDDLEWARE
-server.use(helmet()) //security
-server.use(cors()) //ensures front and back end can work from the same machine
-server.use(express.json()) //json all the things!
+server.use(helmet()); //security
+server.use(cors()); //ensures front and back end can work from the same machine
+server.use(express.json()); //json all the things!
 
 //ROUTES
-server.use('/api/users', routesUsers)
-server.use('/api/exercises', routesExercises)
+server.use("/api/users", routesUsers);
+server.use("/api/exercises", routesExercises);
+server.use("/api/records", routesRecords);
 
 // API is online Notification
 server.get("/", (req, res) => {
-    res.send("<h1>API is online</h1>");
-  });
+  res.send("<h1>API is online</h1>");
+});
 
 //EXPORTS
-module.exports = server
+module.exports = server;
