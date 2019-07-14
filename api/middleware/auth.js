a91cd574f5c7b71815d7e135310e3e4ae2839202
dustinsnoap <afrobacon@gmail.com>
2019-07-14 00:01:37 -0400

    Fixed login route All user routes are currently working now :)

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -24,8 +24,10 @@ authenticate = async (req, res, next) => {
 }
 username_exists = async (req, res, next) => {
     const user = await get.user_by_username(req.body.username)
-    if(user) req.body.user = user
-    else res.status(404).json({message: `Username ${req.body.username} couldn't be found.`})
+    if(user)
+        req.body.user = user
+    else
+        return res.status(404).json({message: `Username ${req.body.username} couldn't be found.`})
 
     next()
 }
@@ -33,7 +35,7 @@ password_matches = async (req, res, next) => {
     if(crypt.compareSync(req.body.password, req.body.user.password))
         req.authorization = jwtGenToken(req.body.user)
     else
-        res.status(403).json({message: `Incorrect password dumbass.`})
+        return res.status(403).json({message: `Incorrect password dumbass.`})
 
     next()
 }
