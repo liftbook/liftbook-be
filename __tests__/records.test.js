6fa758a47f61885d976d09af411bec269b1b276a
dustinsnoap <afrobacon@gmail.com>
2019-07-16 22:44:38 -0400

    recorded record tests

--- __tests__/records.test.js
+++ __tests__/records.test.js
@@ -1,10 +1,10 @@
-// ___      _______  _______    _______  _______  _______  _______  _______ 
-//|   |    |       ||       |  |       ||       ||       ||       ||       |
-//|   |    |   _   ||    ___|  |_     _||    ___||  _____||_     _||  _____|
-//|   |    |  | |  ||   | __     |   |  |   |___ | |_____   |   |  | |_____ 
-//|   |___ |  |_|  ||   ||  |    |   |  |    ___||_____  |  |   |  |_____  |
-//|       ||       ||   |_| |    |   |  |   |___  _____| |  |   |   _____| |
-//|_______||_______||_______|    |___|  |_______||_______|  |___|  |_______| 
+// ______    _______  _______  _______  ______    ______     _______  _______  _______  _______  _______ 
+//|    _ |  |       ||       ||       ||    _ |  |      |   |       ||       ||       ||       ||       |
+//|   | ||  |    ___||       ||   _   ||   | ||  |  _    |  |_     _||    ___||  _____||_     _||  _____|
+//|   |_||_ |   |___ |       ||  | |  ||   |_||_ | | |   |    |   |  |   |___ | |_____   |   |  | |_____ 
+//|    __  ||    ___||      _||  |_|  ||    __  || |_|   |    |   |  |    ___||_____  |  |   |  |_____  |
+//|   |  | ||   |___ |     |_ |       ||   |  | ||       |    |   |  |   |___  _____| |  |   |   _____| |
+//|___|  |_||_______||_______||_______||___|  |_||______|     |___|  |_______||_______|  |___|  |_______|
 
 //IMPORTS
 const request = require('supertest')
@@ -12,14 +12,15 @@ const server = require('../api/server')
 const db = require('../data/config')
 
 //ROUTES
-const route_logs = '/api/logs'
+const route_records = '/api/records'
 const route_users = '/api/users'
 const route_register = route_users + '/register'
 const route_exercises = '/api/exercises'
 
-xdescribe('logs', () => {
+//TESTS
+describe('records', () => {
     describe('add', () => {
-        afterEach(async () => await db('logs').truncate())
+        afterEach(async () => await db('records').truncate())
 
         it('should receive status 201', async () => {
             //add user
@@ -29,11 +30,11 @@ xdescribe('logs', () => {
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
 
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip'}
-            const res = await request(server).post(route_logs).send(log)
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip'}
+            const res = await request(server).post(route_records).send(record)
             expect(res.status).toBe(201)
         })
-        it('should receive log back', async () => {
+        it('should receive record back', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
@@ -41,8 +42,8 @@ xdescribe('logs', () => {
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
 
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            const res = await request(server).post(route_logs).send(log)
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            const res = await request(server).post(route_records).send(record)
             expect(res.body.weight).toBe('3000')
         })
         it('should receive status 500 with missing fields', async () => {
@@ -53,13 +54,13 @@ xdescribe('logs', () => {
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
 
-            const log = {lid: '1', username: 'gordon'}
-            const res = await request(server).post(route_logs).send(log)
+            const record = {rid: '1', username: 'gordon', weight: '3000'}
+            const res = await request(server).post(route_records).send(record)
             expect(res.status).toBe(612)
         })
     })
     describe('get', () => {
-        afterEach(async () => await db('logs').truncate())
+        afterEach(async () => await db('records').truncate())
 
         it('should receive status 200', async () => {
             //add user
@@ -68,47 +69,58 @@ xdescribe('logs', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add log
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_logs).send(log)
+            //add record
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record)
 
-            const res = await request(server).get(route_logs)
+            const res = await request(server).get(route_records)
             expect(res.status).toBe(200)
         })
-        it('should receive array of logs', async () => {
+        it('should receive array of records', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add goal
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_logs).send(log)
+            //add record
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record)
 
-            const res = await request(server).get(route_logs)
+            const res = await request(server).get(route_records)
             expect(res.body).toHaveLength(1)
         })
-        it('should receive requested log', async () => {
+        it('should receive array of user records', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add log
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_logs).send(log)
-            //get hashed log id
-            let res = await request(server).get(route_logs)
-            const lid = res.body[0].lid
+            //add record
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record)
 
-            res = await request(server).get(`${route_logs}/${lid}`)
-            expect(res.body.weight).toBe('3000')
+            res = await request(server).get(`${route_records}/${user.username}`)
+            expect(res.body).toHaveLength(1)
+        })
+        it('should receive array of user records for exercise', async () => {
+            //add user
+            const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
+            await request(server).post(route_register).send(user)
+            //add exercise
+            const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
+            await request(server).post(route_exercises).send(exercise)
+            //add record
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record)
+
+            res = await request(server).get(`${route_records}/${user.username}/${exercise.name}`)
+            expect(res.body).toHaveLength(1)
         })
     })
     describe('update', () => {
-        afterEach(async () => await db('logs').truncate())
+        afterEach(async () => await db('records').truncate())
         it('should receive status 201', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
@@ -116,15 +128,15 @@ xdescribe('logs', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add log
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_logs).send(log)
-            //get hashed log id
-            let res = await request(server).get(route_logs)
-            const lid = res.body[0].lid
+            //add record
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record)
+            //get hashed record id
+            let res = await request(server).get(route_records)
+            const rid = res.body[0].rid
 
             const update = {weight: '10000'}
-            res = await request(server).put(`${route_logs}/${lid}`).send(update)
+            res = await request(server).put(`${route_records}/${rid}`).send(update)
             expect(res.status).toBe(200)
         })
         it('should return updated object', async () => {
@@ -134,20 +146,20 @@ xdescribe('logs', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add log
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_logs).send(log)
-            //get hashed log id
-            let res = await request(server).get(route_logs)
-            const lid = res.body[0].lid
-    
+            //add record
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record)
+            //get hashed record id
+            let res = await request(server).get(route_records)
+            const rid = res.body[0].rid
+
             const update = {weight: '10000'}
-            res = await request(server).put(`${route_logs}/${lid}`).send(update)
+            res = await request(server).put(`${route_records}/${rid}`).send(update)
             expect(res.body.weight).toBe('10000')
         })
     })
     describe('remove', () => {
-        afterEach(async () => await db('logs').truncate())
+        afterEach(async () => await db('records').truncate())
         it('should return status 200', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
@@ -155,34 +167,34 @@ xdescribe('logs', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add log
-            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_logs).send(log)
-            //get hashed log id
-            let res = await request(server).get(route_logs)
-            const lid = res.body[0].lid
+            //add record
+            const record = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record)
+            //get hashed record id
+            let res = await request(server).get(route_records)
+            const rid = res.body[0].rid
 
-            res = await request(server).delete(`${route_logs}/${lid}`)
+            res = await request(server).delete(`${route_records}/${rid}`)
             expect(res.status).toBe(200)
         })
-        it('should remove log from db', async () => {
+        it('should remove record from db', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add log
-            const log1 = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            const log2 = {lid: '2', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_logs).send(log1)
-            await request(server).post(route_logs).send(log2)
-            //get hashed log id
-            let res = await request(server).get(route_logs)
-            const lid = res.body[0].lid
+            //add record
+            const record1 = {rid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            const record2 = {rid: '2', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_records).send(record1)
+            await request(server).post(route_records).send(record2)
+            //get hashed record id
+            let res = await request(server).get(route_records)
+            const rid = res.body[0].rid
 
-            await request(server).delete(`${route_logs}/${lid}`)
-            res = await request(server).get(route_logs)
+            await request(server).delete(`${route_records}/${rid}`)
+            res = await request(server).get(route_records)
             expect(res.body).toHaveLength(1)
         })
     })
