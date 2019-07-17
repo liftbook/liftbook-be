41a5406ceccf57446edd9674ff5ff074eb0e60da
dustinsnoap <afrobacon@gmail.com>
2019-07-16 22:16:23 -0400

    drop a few log tests

--- __tests__/logs.test.js
+++ __tests__/logs.test.js
@@ -1,10 +1,10 @@
-// _______  _______  _______  ___        _______  _______  _______  _______  _______ 
-//|       ||       ||   _   ||   |      |       ||       ||       ||       ||       |
-//|    ___||   _   ||  |_|  ||   |      |_     _||    ___||  _____||_     _||  _____|
-//|   | __ |  | |  ||       ||   |        |   |  |   |___ | |_____   |   |  | |_____ 
-//|   ||  ||  |_|  ||       ||   |___     |   |  |    ___||_____  |  |   |  |_____  |
-//|   |_| ||       ||   _   ||       |    |   |  |   |___  _____| |  |   |   _____| |
-//|_______||_______||__| |__||_______|    |___|  |_______||_______|  |___|  |_______| 
+// ___      _______  _______    _______  _______  _______  _______  _______ 
+//|   |    |       ||       |  |       ||       ||       ||       ||       |
+//|   |    |   _   ||    ___|  |_     _||    ___||  _____||_     _||  _____|
+//|   |    |  | |  ||   | __     |   |  |   |___ | |_____   |   |  | |_____ 
+//|   |___ |  |_|  ||   ||  |    |   |  |    ___||_____  |  |   |  |_____  |
+//|       ||       ||   |_| |    |   |  |   |___  _____| |  |   |   _____| |
+//|_______||_______||_______|    |___|  |_______||_______|  |___|  |_______| 
 
 //IMPORTS
 const request = require('supertest')
@@ -12,15 +12,15 @@ const server = require('../api/server')
 const db = require('../data/config')
 
 //ROUTES
+const route_logs = '/api/logs'
 const route_goals = '/api/goals'
 const route_users = '/api/users'
 const route_register = route_users + '/register'
 const route_exercises = '/api/exercises'
 
-//TESTS
-xdescribe('goals', () => {
+describe('logs', () => {
     describe('add', () => {
-        afterEach(async () => await db('goals').truncate())
+        afterEach(async () => await db('logs').truncate())
 
         it('should receive status 201', async () => {
             //add user
@@ -30,11 +30,11 @@ xdescribe('goals', () => {
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
 
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip'}
-            const res = await request(server).post(route_goals).send(goal)
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip'}
+            const res = await request(server).post(route_logs).send(log)
             expect(res.status).toBe(201)
         })
-        it('should receive goal back', async () => {
+        it('should receive log back', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
@@ -42,8 +42,8 @@ xdescribe('goals', () => {
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
 
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            const res = await request(server).post(route_goals).send(goal)
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            const res = await request(server).post(route_logs).send(log)
             expect(res.body.weight).toBe('3000')
         })
         it('should receive status 500 with missing fields', async () => {
@@ -54,13 +54,13 @@ xdescribe('goals', () => {
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
 
-            const goal = {gid: '1', username: 'gordon', weight: '3000'}
-            const res = await request(server).post(route_goals).send(goal)
+            const log = {lid: '1', username: 'gordon'}
+            const res = await request(server).post(route_logs).send(log)
             expect(res.status).toBe(612)
         })
     })
     describe('get', () => {
-        afterEach(async () => await db('goals').truncate())
+        afterEach(async () => await db('logs').truncate())
 
         it('should receive status 200', async () => {
             //add user
@@ -69,14 +69,14 @@ xdescribe('goals', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add goal
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_goals).send(goal)
+            //add log
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_logs).send(log)
 
-            const res = await request(server).get(route_goals)
+            const res = await request(server).get(route_logs)
             expect(res.status).toBe(200)
         })
-        it('should receive array of goals', async () => {
+        it('should receive array of logs', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
@@ -84,32 +84,32 @@ xdescribe('goals', () => {
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
             //add goal
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_goals).send(goal)
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_logs).send(log)
 
-            const res = await request(server).get(route_goals)
+            const res = await request(server).get(route_logs)
             expect(res.body).toHaveLength(1)
         })
-        it('should receive requested goal', async () => {
+        it('should receive requested log', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add goal
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_goals).send(goal)
-            //get hashed goal id
-            let res = await request(server).get(route_goals)
-            const gid = res.body[0].gid
+            //add log
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_logs).send(log)
+            //get hashed log id
+            let res = await request(server).get(route_logs)
+            const lid = res.body[0].lid
 
-            res = await request(server).get(`${route_goals}/${gid}`)
+            res = await request(server).get(`${route_logs}/${lid}`)
             expect(res.body.weight).toBe('3000')
         })
     })
     describe('update', () => {
-        afterEach(async () => await db('goals').truncate())
+        afterEach(async () => await db('logs').truncate())
         it('should receive status 201', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
@@ -117,15 +117,15 @@ xdescribe('goals', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add goal
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_goals).send(goal)
-            //get hashed goal id
-            let res = await request(server).get(route_goals)
-            const gid = res.body[0].gid
+            //add log
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_logs).send(log)
+            //get hashed log id
+            let res = await request(server).get(route_logs)
+            const lid = res.body[0].lid
 
             const update = {weight: '10000'}
-            res = await request(server).put(`${route_goals}/${gid}`).send(update)
+            res = await request(server).put(`${route_logs}/${lid}`).send(update)
             expect(res.status).toBe(200)
         })
         it('should return updated object', async () => {
@@ -135,20 +135,20 @@ xdescribe('goals', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add goal
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_goals).send(goal)
-            //get hashed goal id
-            let res = await request(server).get(route_goals)
-            const gid = res.body[0].gid
-
+            //add log
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_logs).send(log)
+            //get hashed log id
+            let res = await request(server).get(route_logs)
+            const lid = res.body[0].lid
+    
             const update = {weight: '10000'}
-            res = await request(server).put(`${route_goals}/${gid}`).send(update)
+            res = await request(server).put(`${route_logs}/${lid}`).send(update)
             expect(res.body.weight).toBe('10000')
         })
     })
     describe('remove', () => {
-        afterEach(async () => await db('goals').truncate())
+        afterEach(async () => await db('logs').truncate())
         it('should return status 200', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
@@ -156,34 +156,34 @@ xdescribe('goals', () => {
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add goal
-            const goal = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_goals).send(goal)
-            //get hashed goal id
-            let res = await request(server).get(route_goals)
-            const gid = res.body[0].gid
+            //add log
+            const log = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_logs).send(log)
+            //get hashed log id
+            let res = await request(server).get(route_logs)
+            const lid = res.body[0].lid
 
-            res = await request(server).delete(`${route_goals}/${gid}`)
+            res = await request(server).delete(`${route_logs}/${lid}`)
             expect(res.status).toBe(200)
         })
-        it('should remove goal from db', async () => {
+        it('should remove log from db', async () => {
             //add user
             const user = {uid: '1', username: 'gordon', password: 'bacon', email: 'kacon@bevin', first_name: 'kevin', last_name: 'bacon'}
             await request(server).post(route_register).send(user)
             //add exercise
             const exercise = {eid: '1', name: 'backflip', username: 'gordon', description: 'nope'}
             await request(server).post(route_exercises).send(exercise)
-            //add goal
-            const goal1 = {gid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            const goal2 = {gid: '2', username: 'gordon', exercise: 'backflip', weight: '3000'}
-            await request(server).post(route_goals).send(goal1)
-            await request(server).post(route_goals).send(goal2)
-            //get hashed goal id
-            let res = await request(server).get(route_goals)
-            const gid = res.body[0].gid
+            //add log
+            const log1 = {lid: '1', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            const log2 = {lid: '2', username: 'gordon', exercise: 'backflip', weight: '3000'}
+            await request(server).post(route_logs).send(log1)
+            await request(server).post(route_logs).send(log2)
+            //get hashed log id
+            let res = await request(server).get(route_logs)
+            const lid = res.body[0].lid
 
-            await request(server).delete(`${route_goals}/${gid}`)
-            res = await request(server).get(route_goals)
+            await request(server).delete(`${route_logs}/${lid}`)
+            res = await request(server).get(route_logs)
             expect(res.body).toHaveLength(1)
         })
     })
