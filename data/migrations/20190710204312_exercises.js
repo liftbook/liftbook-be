000f808e4c669292fa8ebaa977e8ebecb2de7075
Mychal <hallmychal@gmail.com>
2019-07-16 23:04:22 -0500

    adjusted exercise for FE

--- data/migrations/20190710204312_exercises.js
+++ data/migrations/20190710204312_exercises.js
@@ -4,23 +4,27 @@ exports.up = knex =>
       .string("eid")
       .primary()
       .unique()
-      .notNullable()
-    tbl.string('created_by')
-      .references('uid')
-      .inTable('users')
-      .notNullable()
-    tbl.string('updated_by')
-      .references('uid')
-      .inTable('users')
-      .notNullable()
+      .notNullable();
+    tbl
+      .string("created_by")
+      .references("uid")
+      .inTable("users")
+      .notNullable();
+    tbl
+      .string("updated_by")
+      .references("uid")
+      .inTable("users")
+      .notNullable();
     tbl
       .string("name")
       .unique()
       .notNullable();
-    tbl.string("description").notNullable();
+    tbl.string("weight_lifted").notNullable();
+    tbl.string("repetitions");
+    tbl.string("body_part");
+    tbl.string("date");
     tbl.string("icon_src");
     tbl.timestamps(true, true);
-  })
+  });
 
-exports.down = (knex) =>
-  knex.schema.dropTableIfExists("exercises")
+exports.down = knex => knex.schema.dropTableIfExists("exercises");
