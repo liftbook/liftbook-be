38b15e272df328e39fe85a827d99e9b5e43c130b
Mychal <hallmychal@gmail.com>
2019-07-17 13:44:28 -0500

    functional

--- api/server.js
+++ api/server.js
@@ -3,12 +3,12 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 //routes
-const routesLogs = require('./routes/logs')
+const routesLogs = require("./routes/logs");
 const routesUsers = require("./routes/users");
 const routesExercises = require("./routes/exercises");
 const routesRecords = require("./routes/records");
-const routesGoals = require('./routes/goals')
-
+const routesGoals = require("./routes/goals");
+const routesPhotos = require("./routes/photos");
 
 //SETUP
 const server = express();
@@ -20,17 +20,17 @@ server.use(express.json()); //json all the things!
 
 //ROUTES
 
-server.use('/api/logs', routesLogs)
+server.use("/api/logs", routesLogs);
 server.use("/api/users", routesUsers);
 server.use("/api/exercises", routesExercises);
 server.use("/api/records", routesRecords);
-server.use('/api/goals', routesGoals)
+server.use("/api/goals", routesGoals);
+server.use("/api/photos", routesPhotos);
 
 // API is online Notification
 server.get("/", (req, res) => {
   res.send("<h1>API is online</h1>");
 });
-
 
 //EXPORTS
 module.exports = server;
